e35dbf7b91d04721ec544563da53e01767310f63
Sowrya-11 <76764710+Sowrya-11@users.noreply.github.com>
2021-11-25 10:07:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var reasons=[
+    "2.Sit up straight and lean slightly  forward. Pinch your nostrils between your thumb and forefinger <br> for about 10 minutes. <br> This apply pressure on the bleeding portion.",
+    "3. Breathe thorugh the mouth and wait till the bleeding stops. This normally Taks 7 - 8 minutes.",
+    "4. You can also prepare an ice pack by warpping ice cubes in a cloth and crushing the ice. <br> This Will also reduce Bleeding",
+    "Thank You!, Refresh to Go To The First Bit!",
+    
+  ];
+
+  var i=0;
+  function change(){
+    document.getElementById("reason").innerHTML=reasons[i];
+    i++;
+  }
+
